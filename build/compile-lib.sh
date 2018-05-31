71fefde4515699d58323ba5313f17cfe5eb924e3
p-sam <p-sam@d3vs.net>
2018-05-31 15:34:08 +0200

    fix build error when the "out" dir is not yet created

--- build/compile-lib.sh
+++ build/compile-lib.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.." || exit 1
 
+mkdir -p "$PROJECT_DIR/out"
+
 exec clang -dynamiclib -std=gnu99 "$PROJECT_DIR/iohid_wrap.m" -current_version 1.0 -compatibility_version 1.0 -lobjc -framework Foundation -framework AppKit -framework CoreFoundation -o "$PROJECT_DIR/out/iohid_wrap.dylib"
